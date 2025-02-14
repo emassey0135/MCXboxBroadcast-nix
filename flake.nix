f397ac4df89f678dc0120398abd0df78a3e806a9
Elijah Massey <emassey0135@gmail.com>
2025-02-13 18:01:38 -0600

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-24.11";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem(system:
+      let pkgs = import nixpkgs {
+        inherit system;
+      };
+        mcxboxbroadcast = (with pkgs;
+          stdenv.mkDerivation {
+            pname = "mcxboxbroadcast";
+            version = "1.0";
+            src = fetchurl {
+              url = "https://github.com/MCXboxBroadcast/Broadcaster/releases/download/73/MCXboxBroadcastStandalone.jar";
+              hash = "sha256-YB3heS+xTRshSBzIfCngtIXvZnDlz7l3vmmgqBkYqDU=";
+            };
+            phases = ["installPhase"];
+            installPhase = ''
+              mkdir -p $out/bin
+              cp $src $out/bin/MCXboxBroadcastStandalone.jar
+            '';
+          }
+        );
+      in rec {
+        defaultApp = flake-utils.lib.mkApp {
+          drv = defaultPackage;
+        };
+        defaultPackage = mcxboxbroadcast;
+      }
+    );
+}
